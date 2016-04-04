0590fd6ed0cd04c3a757b5baeb060c0f4611e3bd
Real De Cynthia <giginyareal@gmail.com>
2016-04-05 03:56:09 +0700

    Added files via upload

---
+++ BelajarArray.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package belajararray;
+
+/**
+ *
+ * @author USER
+ */
+public class BelajarArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] data = {20, 10, 50, 30, 10};
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Data ke " + i + " adalah : " + data[i]);
+        }
+        // TODO code application logic here
+    }
+    
+}
